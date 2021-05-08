16b8ae4b6bc6abef95f7c6f65fcd913b341408ec
Nur-allhi <noorefty1@gmail.com>
2021-05-08 17:33:25 +0600

    quantity issue fixed

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,27 +1,50 @@
 import React, { useEffect, useState } from "react";
 import fakeData from "../../fakeData";
-import { getDatabaseCart } from "../../utilities/databaseManager";
+import {
+  getDatabaseCart,
+  removeFromDatabaseCart,
+} from "../../utilities/databaseManager";
+import Cart from "../Cart/Cart";
 import ReviewIteam from "../ReviewIteam/ReviewIteam";
 
 const Review = () => {
   const [cart, setCart] = useState([]);
+
+  const removeProduct = (productkey) => {
+    // console.log("remove clicked", productkey);
+    const newCart = cart.filter((pd) => pd.key !== productkey);
+    setCart(newCart);
+    removeFromDatabaseCart(productkey);
+  };
+
   useEffect(() => {
     // Cart
     const savedCart = getDatabaseCart();
     const productKeys = Object.keys(savedCart);
     const cartProducts = productKeys.map((key) => {
       const product = fakeData.find((pd) => pd.key === key);
-      product.quantity = savedCart[key];
+      const quantity = savedCart[key];
+
+      product.quantity = quantity;
       return product;
     });
+
     setCart(cartProducts);
   }, []);
   return (
-    <div>
-      <h1>Cart iteams {cart.length}</h1>
-      {cart.map((pd) => (
-        <ReviewIteam product={pd} key={pd.key}></ReviewIteam>
-      ))}
+    <div className="twin-container">
+      <div className="product-container">
+        {cart.map((pd) => (
+          <ReviewIteam
+            product={pd}
+            key={pd.key}
+            removeProduct={removeProduct}
+          ></ReviewIteam>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
--- src/components/ReviewIteam/ReviewIteam.js
+++ src/components/ReviewIteam/ReviewIteam.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./ReviewItem.css";
 
 const ReviewIteam = (props) => {
-  const { name, quantity } = props.product;
+  const { name, quantity, key, price } = props.product;
   return (
     <div className="review-iteam">
       <h4 className="product-name">{name}</h4>
       <p>Quantity: {quantity}</p>
+      <p>Price: {price}</p>
       <br />
-      <button className="add-cart-btn">Remove</button>
+      <button onClick={() => props.removeProduct(key)} className="add-cart-btn">
+        Remove
+      </button>
     </div>
   );
 };
